2a5b75ff30cc7633d5328e0e8d6485bf4fd1d7a8
... <37700859+453594848@users.noreply.github.com>
2024-06-13 12:02:30 +0800

    集成mybatis

--- member/src/main/java/com/wang/train/member/controller/TestController.java
+++ member/src/main/java/com/wang/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.wang.train.member.Controller;
+package com.wang.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
